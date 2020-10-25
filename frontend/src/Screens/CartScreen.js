36159e52107b756e4651f3c27f71544d07c03c73
Vasudha Tapriya <vasudhatapriya2@gmail.com>
2020-10-25 17:03:22 +0530

    chgnd

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -1,4 +1,6 @@
 import React,{useEffect} from "react";
+import {useDispatch} from 'react-redux';
+import {addToCart} from '../actions/cartActions';
 function CartScreen(props){
     const productId=props.match.param.id;
     const qty=props.location.search? Number(props.location.search.split("=")[1]):1;
--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,4 +1,6 @@
+import { CART_ADD_ITEM } from "../constants/cartConstants";
 const { default: Axios } = require("axios")
+
 
 const addToCart=(productId, qty)=> async (dispatch)=>{
     try{
@@ -7,8 +9,8 @@ const addToCart=(productId, qty)=> async (dispatch)=>{
             type: CART_ADD_ITEM, payload:{
             product: data._id,
             name: data.name,
-            image=data.image,
-            price=data.price,
+            image:data.image,
+            price:data.price,
             countInStock: data.countInStock,
             qty
 
